5113bcda73b4b03f2f01da7e3e1dcf12cfd9f544
Kevin Mullin <kevin@kpmullin.com>
2012-03-15 17:08:09 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkBox::Application.config.secret_token = '84098673a48da90bc56ca99ec3b87ae74d761ca01de36c610af55c811b2a829680936e22d6c460e7cb6438bfc7fdc64332c06e642bb9232876004a1a36f56313'
