9b681b4579e6ee419c2afc7ce8a18b153c10c84d
Kevin Mullin <kevin@kpmullin.com>
2012-03-18 22:55:34 -0700

    removed unneeded files, working download referer page

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -7,11 +7,12 @@ class AssetsController < ApplicationController
 
   def download
     @asset = ShortUrl.find_by_link_id!(params[:link_id]).asset
+    @link_id = params[:link_id]
     head(
-      :x_accel_redirect => @asset.path + '/' + @asset.filename,
-      :content_type => 'application/stream',
+      :x_accel_redirect => '/files/' + @asset.filename,
+      :content_type => @asset.content_type || 'application/stream',
       :content_disposition => "attachment; filename=\"#{@asset.filename}\""
-    )
+    ) if request.referrer == "http://localhost:3000#{ShortUrl.full_url(params[:link_id])}"
   end
 
 end
